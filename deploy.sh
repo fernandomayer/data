e6e77e8c1957ee36d04be094c3453d2c751e4346
Fernando Mayer <fernandomayer@gmail.com>
2019-05-22 22:19:26 -0300

    Inclui porta no deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 USER=fernandomayer
 HOST=leg.ufpr.br
 DIR=public_html/data/
+read -p 'PORTA: ' PORT
 
-Rscript -e 'library(rmarkdown);render("index.Rmd")' && rsync -avz --delete . ${USER}@${HOST}:~/${DIR}
+Rscript -e 'library(rmarkdown);render("index.Rmd")' && rsync -avz -e "ssh -p $PORT" --delete . ${USER}@${HOST}:~/${DIR}
 
 exit 0
